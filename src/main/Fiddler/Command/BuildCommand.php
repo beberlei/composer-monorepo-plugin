09f40326f2587d5c7ea558adddaeb3ff05af3c35
Benjamin Eberlei <kontakt@beberlei.de>
2015-04-10 13:25:10 +0200

    Add console command

---
+++ src/main/Fiddler/Command/BuildCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Fiddler\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+use Composer\IO\ConsoleIO;
+
+use Fiddler\Build;
+
+class BuildCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('build')
+            ->setDescription('Fiddler Build step generates all autoloaders for all components.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $build = new Build(new ConsoleIO($input, $output, $this->getHelperSet()));
+    }
+}
