2b2b126fc764cf51e59be2678d9a4961e8a525b8
Andy Thorne <contrabandvr@gmail.com>
2020-11-30 03:03:07 +0000

    Use the correct file package key so composer requires cache works

--- src/main/Monorepo/Composer/AutoloadGenerator.php
+++ src/main/Monorepo/Composer/AutoloadGenerator.php
@@ -17,27 +17,15 @@ class AutoloadGenerator extends \Composer\Autoload\AutoloadGenerator
         return $packageMap;
     }
 
-    protected function getAutoloadRealFile($useClassMap, $useIncludePath, $targetDirLoader, $useIncludeFiles, $vendorPathCode, $appBaseDirCode, $suffix, $useGlobalIncludePath, $prependAutoloader, $staticPhpVersion = 70000, $checkPlatform)
+    protected function getFileIdentifier(PackageInterface $package, $path)
     {
-        $file = parent::getAutoloadRealFile($useClassMap, $useIncludePath, $targetDirLoader, false, $vendorPathCode, $appBaseDirCode, $suffix, $useGlobalIncludePath, $prependAutoloader, $staticPhpVersion, $checkPlatform);
+        $extra = $package->getExtra();
 
-        if (! $useIncludeFiles) {
-            return $file;
-        }
-
-        return $file .= <<<INCLUDE_FILES
-
-\$includeFiles = require __DIR__ . '/autoload_files.php';
-foreach (\$includeFiles as \$file) {
-    composerRequireOnce$suffix(\$file);
-}
-
-function composerRequireOnce$suffix(\$file)
-{
-    require_once \$file;
-}
-
-INCLUDE_FILES;
+        return md5(
+            $extra['monorepo']['original_name'] .
+            ':' .
+            $path
+        );
     }
 
     protected function filterPackageMap(array $packageMap, RootPackageInterface $mainPackage)
